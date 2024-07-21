f8564c61fc69813696e03bd213eb6173cf0f000f
lucdev <me@lucdev.net>
2024-07-21 20:20:48 -0300

    Setup eslint + lefthook

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true
+  },
+  plugins: ["prettier", "@typescript-eslint", "astro"],
+  extends: [
+    "plugin:astro/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked"
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports"
+      }
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_"
+      }
+    ],
+    "@typescript-eslint/require-await": "off",
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksVoidReturn: {
+          attributes: false
+        }
+      }
+    ]
+  },
+  ignorePatterns: [
+    ".github/",
+    ".vscode/",
+    ".idx/",
+    ".husky/",
+    ".astro",
+    "dist/",
+    "node_modules/",
+    "public/",
+    ".*.cjs",
+    "*.config.mjs",
+    "*.yaml",
+    "*.json"
+  ]
+};
+module.exports = config;
