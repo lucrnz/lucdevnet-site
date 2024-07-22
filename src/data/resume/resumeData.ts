4b0221321947393b2b0f5286f4ae5800c6d0a3e7
lucdev <me@lucdev.net>
2024-07-22 20:07:47 -0300

    wip: New resume - delete resume data file

--- src/data/resume/resumeData.ts
+++
@@ -1,8 +0,0 @@
-import type { ResumeData } from "~/types/resume/ResumeData";
-import { resumeCertifications } from "./certifications";
-import { resumeExperience } from "./experience";
-
-export const resumeData: ResumeData = {
-  experience: resumeExperience,
-  certifications: resumeCertifications
-};
