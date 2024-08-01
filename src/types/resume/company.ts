c3296fed851ec039d75f8b8d462ece8d481635de
lucdev <me@lucdev.net>
2024-08-01 12:10:24 -0300

    Starting a position on Globant as front-end developer!

--- src/types/resume/company.ts
+++ src/types/resume/company.ts
@@ -1,5 +1,6 @@
 export enum JobCompany {
   cognizant = "Cognizant Softvision",
   kimn = "Kimn Consulting",
-  free = "Freelancer - My projects"
+  free = "Entrepeneur - My projects",
+  globant = "Globant"
 }
