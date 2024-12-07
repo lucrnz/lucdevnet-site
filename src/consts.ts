1b55c9f0693a9c703573e5088f702cffc2d0c02c
lucdev <me@lucdev.net>
2024-12-07 03:42:44 +0000

    update profile picture 😎

--- src/consts.ts
+++ src/consts.ts
@@ -20,8 +20,8 @@ export const authorDescription = [
   "I enjoy sharing what I learn with the community and help others build 🙂"
 ];
 
-export const authorProfilePicture = "/images/author/lhillcoat_picture.webp";
+export const authorProfilePicture = "/images/author/lhillcoat_picture_v2.webp";
 export const authorProfilePictureSmall =
-  "/images/author/lhillcoat_picture_small.webp";
+  "/images/author/lhillcoat_picture_v2_small.webp";
 export const authorProfilePictureAlt = `${authorName[0]} photo, a person with short hair and glasses`;
 export const xUserName = "lucrnz";
