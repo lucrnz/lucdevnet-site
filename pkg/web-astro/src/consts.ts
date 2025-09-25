4c14c123867797c81f135dd6f19fc5832edd4fc1
lucdev <me@lucdev.net>
2025-09-25 17:20:17 -0300

    adjust author values

--- pkg/web-astro/src/consts.ts
+++ pkg/web-astro/src/consts.ts
@@ -10,23 +10,24 @@ export const siteCreationDate = new Date("2022-10-01T16:51:12Z");
 export const siteDescription =
   "My personal website including all my projects, contact links and my blog.";
 
-export const siteSourceCodeUrl = "https://git.lucdev.net/lucdev/site";
-
-/* Author values */
-export const authorName = ["Luciano", "Hillcoat"];
-export const authorEmail = "me@lucdev.net";
-export const authorSocialMedia = [
-  "https://linkedin.com/in/lhillcoat",
-  "https://github.com/lucrnz"
-];
-export const authorDescription = [
-  "Software developer, focusing on the web platform",
-  "I enjoy sharing what I learn with the community and help others build 🙂"
-];
+export const siteSourceCodeUrl = "https://github.com/lucdev/site";
 
 export const authorProfilePicture = "/images/author/luc_fursona_v1.webp";
 export const authorProfilePictureSmall =
   "/images/author/luc_fursona_v1_small.webp";
 export const authorProfilePictureAlt = `Luc's fursona, anthropomorphic character red panda-raccoon hybrid with rust-orange fur, short tousled hair, glasses, and a soft smile, shown in a warm indoor setting.`;
-
 export const xUserName = "lucdevnet";
+
+/* Author values */
+export const authorName = ["Luciano", "Hillcoat"];
+export const authorEmail = "me@lucdev.net";
+export const authorSocialMedia = [
+  `https://x.com/${xUserName}`,
+  "https://linkedin.com/in/lhillcoat",
+  "https://github.com/lucrnz"
+];
+
+export const authorDescription = [
+  "Software developer, focusing on the web platform",
+  "I enjoy sharing what I learn with the community and help others build 🙂"
+];
